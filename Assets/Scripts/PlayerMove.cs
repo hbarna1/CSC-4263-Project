ae0497e4f5a82d0e434786b946c7e76725cdec00
Jon-Kris Traylor <traylorjk@gmail.com>
2017-03-16 20:41:36 -0500

    Initialized Project
    Initialized project with a simple sprite and movement script

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float Speed = 0f;
+    private float movex = 0f;
+    private float movey = 0f;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        movex = Input.GetAxis("Horizontal");
+        movey = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(movex * Speed, movey * Speed);
+    }
+}
