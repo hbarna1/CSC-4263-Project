fe8a5d5c150e76ca06163e2977b80eda56b96ffd
Amoses7 <amoses7@lsu.edu>
2017-03-18 10:41:25 -0500

    Add files via upload

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class PlayerController : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+    public float speed;
+    public Boundary boundary;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb2d.velocity = (movement * speed);
+        rb2d.position = new Vector2
+        (
+            Mathf.Clamp(rb2d.position.x, boundary.xMin, boundary.xMax),
+            Mathf.Clamp(rb2d.position.y, boundary.yMin, boundary.yMax)
+        );
+    }
+}
