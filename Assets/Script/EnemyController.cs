8d5e8278bfb2a928fdc387817d59f559e4cfb9ad
Jon-Kris Traylor <traylorjk@gmail.com>
2017-03-20 20:08:11 -0500

    Adds Shooting Enemy Prefab

---
+++ Assets/Script/EnemyController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float fireRate;
+	public Bullet bullet;
+	public Transform EnemyBulletSpawn;
+	public Transform Player;
+	float nextFire = 0;
+
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+		//transform.LookAt (Player);
+
+		if (Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+
+			Instantiate (bullet, EnemyBulletSpawn.position, EnemyBulletSpawn.rotation);
+		}
+
+
+	}
+}
