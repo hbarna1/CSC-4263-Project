6e845bb062fd1ed9c62a3a20611e3f409c1f2674
Jon-Kris Traylor <traylorjk@gmail.com>
2017-04-22 10:51:56 -0500

    MudPuddles now only destroy player

--- Assets/Script/MudPuddle.cs
+++ Assets/Script/MudPuddle.cs
@@ -6,23 +6,27 @@ public class MudPuddle : MonoBehaviour {
 
 	private Rigidbody2D mudPuddle;  
 	public Boots boots;
+	GameObject player;
 	// Use this for initialization
 	void Start () {
-		GameObject player = GameObject.Find ("Player");
+		player = GameObject.Find ("Player");
 		boots = player.GetComponent<Boots> ();
 
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
 	void OnTriggerEnter2D(Collider2D other){
-		if (boots.hasBoots == false) {
-			// needs to cause death for player
-			Destroy (other.gameObject);
-		}
+		if (other.gameObject.tag == "Player") {
+			
+			if (boots.hasBoots == false) {
+				// needs to cause death for player
 
+				Destroy (other.gameObject);
+			}
+		}
 	}
 }
