8ad6ff436fa5e95bf8cc134118e8d48f6e885d09
Hunter Barnaba <random.blue.mario@gmail.com>
2017-04-23 13:27:22 -0500

    Add files via upload
    Prototype auto-scrolling script, further testing needed with longer levels and interaction with various objects.
    Currently pans camera on loop, includes adjustable panning speed.

---
+++ Assets/Script/Scrolling.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    [SerializeField]
+    private float speed = 800.0f;
+
+    private Vector3 position;
+    private float screenRadius;
+
+    private void Awake()
+    {
+        Application.targetFrameRate = 60;
+
+        this.position = new Vector3();
+        this.screenRadius = this.GetComponent<Camera>().orthographicSize * this.GetComponent<Camera>().aspect;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        this.position = this.transform.position;
+
+        this.position.x += this.speed * Time.deltaTime;
+
+        if (this.position.x > this.screenRadius)
+            this.position.x = -this.screenRadius;
+
+        this.transform.position = this.position;
+    }
+}
