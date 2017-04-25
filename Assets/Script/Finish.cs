50153bf97b1382563a27cde9bbbdcdf37f4876f2
Amoses7 <amoses7@lsu.edu>
2017-04-24 21:05:00 -0500

    Added finish line and victory screen

---
+++ Assets/Script/Finish.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Finish : MonoBehaviour {
+
+    // Use this for initialization
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.gameObject.tag == "Player")
+            SceneManager.LoadScene(2);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
