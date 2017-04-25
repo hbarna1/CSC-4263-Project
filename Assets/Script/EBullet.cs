9a75943c71943891f829186319340a3300d0e499
Hunter Barnaba <random.blue.mario@gmail.com>
2017-04-24 21:50:35 -0500

    Add files via upload
    New script for enemy bullets; Bullets will no longer collide with other bullets or delete non-player enemies.
    
    **Requires testing to see if the script is compatible with our enemies, or if a bug where enemies no longer shoot recurs.

---
+++ Assets/Script/EBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EBullet : MonoBehaviour
+{
+
+    private Rigidbody2D bullet;
+    public float speed;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        bullet = gameObject.GetComponent<Rigidbody2D>();
+        bullet.AddForce(new Vector2(speed, 0));
+    }
+
+
+    void OnBecameInvisible()
+    {
+        Destroy(bullet.gameObject);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+            Destroy(bullet.gameObject);
+        }
+        else if (collision.gameObject.tag == "PBullet")
+            ;
+        else
+            Destroy(bullet.gameObject);
+    }
+
+}
