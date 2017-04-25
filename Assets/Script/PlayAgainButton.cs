3b1af987d38559aa47b55b2572b0257fd98aa93f
Amoses7 <amoses7@lsu.edu>
2017-04-24 21:28:01 -0500

    Added play again button

---
+++ Assets/Script/PlayAgainButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayAgainButton : MonoBehaviour {
+
+	// Use this for initialization
+	public void playagainbutton(string scencename)
+    {
+        SceneManager.LoadScene(0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
